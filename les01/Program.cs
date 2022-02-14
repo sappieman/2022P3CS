3f7d50697fe48777ceb4c80c6b609f849ae1ba87
bas klaichid <basklaichid@gmail.com>
2022-02-14 12:40:58 +0100

    first commit

---
+++ les01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace les01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
